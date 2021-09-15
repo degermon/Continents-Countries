3ea96356ff6875778e854287c66dfe776b812f2a
degermon <degermon@gmail.com>
2021-09-15 18:30:24 +0300

    Initial Commit

---
+++ Continents-Countries/Continents_CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Continents_CountriesApp.swift
+//  Continents-Countries
+//
+//  Created by Daniel Šuškevič on 2021-09-15.
+//
+
+import SwiftUI
+
+@main
+struct Continents_CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
